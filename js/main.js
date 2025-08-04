23891191f9b1914c64e039bd63e4a5c0373740c7
Yoshio <ica.ktw090220@gmail.com>
2025-08-05 07:48:01 +0900

    main_20250805_05

--- js/main.js
+++ js/main.js
@@ -27,23 +27,23 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-$(function(){
+$(window).on('load', function() {
   $('.slider').slick({
-    autoplay: true, //自動でスクロール
-    autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
-    speed: 10000, //スライドが流れる速度を設定
-    cssEase: "linear", //スライドの流れ方を等速に設定
-    slidesToShow: 4, //表示するスライドの数
-    swipe: false, // 操作による切り替えはさせない
-    arrows: false, //矢印非表示
-    pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
-    pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    cssEase: "linear",
+    slidesToShow: 4,
+    swipe: false,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
     variableWidth: true,
     responsive: [
       {
         breakpoint: 750,
         settings: {
-          slidesToShow: 3, //画面幅750px以下でスライド3枚表示
+          slidesToShow: 3,
         }
       }
     ]
